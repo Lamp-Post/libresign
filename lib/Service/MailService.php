fa58e575d2ff0742fbe499cb590e396ca53b1910
Remi Heeres <re.miwh@hotmail.com>
2026-05-06 22:43:24 +0000

    Detect language of User or use default language of nextcloud instead of the languages of the user sending the requests
    Signed-off-by: Remi Heeres <re.miwh@hotmail.com>

--- lib/Service/MailService.php
+++ lib/Service/MailService.php
@@ -15,6 +15,8 @@ use OCA\Libresign\Exception\LibresignException;
 use OCP\IAppConfig;
 use OCP\IL10N;
 use OCP\IURLGenerator;
+use OCP\IUserManager;
+use OCP\L10N\IFactory;
 use OCP\Mail\IMailer;
 use Psr\Log\LoggerInterface;
 
@@ -26,10 +28,24 @@ class MailService {
 		private LoggerInterface $logger,
 		private IMailer $mailer,
 		private FileMapper $fileMapper,
+		private IFactory $l10nFactory,
 		private IL10N $l10n,
 		private IURLGenerator $urlGenerator,
 		private IAppConfig $appConfig,
+		private IUserManager $userManager,
 	) {
+	}
+
+	private function getLangForEmail(string $email): IL10N {
+		$users = $this->userManager->getByEmail($email);
+
+		if (!empty($users)) {
+			$lang = $this->l10nFactory->getUserLanguage($users[0]);
+		} else {
+			$lang = $this->l10nFactory->getUserLanguage(null) ?? $this->l10n->getLanguageCode();
+		}
+
+		return $this->l10nFactory->get('libresign', $lang);
 	}
 
 	/**
@@ -46,22 +62,24 @@ class MailService {
 	 * @psalm-suppress MixedMethodCall
 	 */
 	public function notifySignDataUpdated(SignRequest $data, string $email, ?string $description = null): void {
+		$l = $this->getLangForEmail($email);
+
 		$emailTemplate = $this->mailer->createEMailTemplate('settings.TestEmail');
 		// TRANSLATORS The subject of the email that is sent after changes are made to the signature request that may affect something for the signer who will sign the document. Some possible reasons: URL for signature changed (when the URL expires), the person who requested the signature sent a notification
-		$emailTemplate->setSubject($this->l10n->t('LibreSign: Changes into a file for you to sign'));
+		$emailTemplate->setSubject($l->t('LibreSign: Changes into a file for you to sign'));
 		$emailTemplate->addHeader();
-		$emailTemplate->addHeading($this->l10n->t('File to sign'), false);
+		$emailTemplate->addHeading($l->t('File to sign'), false);
 
 		if (!empty($description)) {
 			$emailTemplate->addBodyText($description);
 			$emailTemplate->addBodyText('');
 		}
 
-		$emailTemplate->addBodyText($this->l10n->t('Changes have been made in a file that you have to sign. Access the link below:'));
+		$emailTemplate->addBodyText($l->t('Changes have been made in a file that you have to sign. Access the link below:'));
 		$link = $this->urlGenerator->linkToRouteAbsolute('libresign.page.sign', ['uuid' => $data->getUuid()]);
 		$file = $this->getFileById($data->getFileId());
 		$emailTemplate->addBodyButton(
-			$this->l10n->t('Sign »%s«', [$file->getName()]),
+			$l->t('Sign »%s«', [$file->getName()]),
 			$link
 		);
 		$message = $this->mailer->createMessage();
@@ -83,21 +101,23 @@ class MailService {
 	 * @psalm-suppress MixedMethodCall
 	 */
 	public function notifyUnsignedUser(SignRequest $data, string $email, ?string $description = null): void {
+		$l = $this->getLangForEmail($email);
+
 		$emailTemplate = $this->mailer->createEMailTemplate('settings.TestEmail');
-		$emailTemplate->setSubject($this->l10n->t('LibreSign: There is a file for you to sign'));
+		$emailTemplate->setSubject($l->t('LibreSign: There is a file for you to sign'));
 		$emailTemplate->addHeader();
-		$emailTemplate->addHeading($this->l10n->t('File to sign'), false);
+		$emailTemplate->addHeading($l->t('File to sign'), false);
 
 		if (!empty($description)) {
 			$emailTemplate->addBodyText($description);
 			$emailTemplate->addBodyText('');
 		}
 
-		$emailTemplate->addBodyText($this->l10n->t('There is a document for you to sign. Access the link below:'));
+		$emailTemplate->addBodyText($l->t('There is a document for you to sign. Access the link below:'));
 		$link = $this->urlGenerator->linkToRouteAbsolute('libresign.page.sign', ['uuid' => $data->getUuid()]);
 		$file = $this->getFileById($data->getFileId());
 		$emailTemplate->addBodyButton(
-			$this->l10n->t('Sign »%s«', [$file->getName()]),
+			$l->t('Sign »%s«', [$file->getName()]),
 			$link
 		);
 		$message = $this->mailer->createMessage();
@@ -116,20 +136,22 @@ class MailService {
 	}
 
 	public function notifySignedUser(SignRequest $signRequest, string $email, File $libreSignFile, string $displayName): void {
+		$l = $this->getLangForEmail($email);
+
 		$emailTemplate = $this->mailer->createEMailTemplate('settings.TestEmail');
 		// TRANSLATORS The subject of the email that is sent after a document has been signed by a user. This email is sent to the person who requested the signature.
-		$emailTemplate->setSubject($this->l10n->t('LibreSign: A file has been signed'));
+		$emailTemplate->setSubject($l->t('LibreSign: A file has been signed'));
 		$emailTemplate->addHeader();
-		$emailTemplate->addHeading($this->l10n->t('File signed'), false);
+		$emailTemplate->addHeading($l->t('File signed'), false);
 		// TRANSLATORS The text in the email that is sent after a document has been signed by a user. %s will be replaced with the name of the user who signed the document.
-		$emailTemplate->addBodyText($this->l10n->t('%s signed the document. You can access it using the link below:', [$signRequest->getDisplayName()]));
+		$emailTemplate->addBodyText($l->t('%s signed the document. You can access it using the link below:', [$signRequest->getDisplayName()]));
 		$link = $this->urlGenerator->linkToRouteAbsolute('libresign.page.indexFPath', [
 			'path' => 'validation/' . $libreSignFile->getUuid(),
 		]);
 		$file = $this->getFileById($signRequest->getFileId());
 		$emailTemplate->addBodyButton(
 			// TRANSLATORS The button text in the email that is sent after a document has been signed by a user. %s will be replaced with the name of the file that was signed.
-			$this->l10n->t('View signed file »%s«', [$file->getName()]),
+			$l->t('View signed file »%s«', [$file->getName()]),
 			$link
 		);
 		$message = $this->mailer->createMessage();
@@ -145,13 +167,15 @@ class MailService {
 	}
 
 	public function notifyCanceledRequest(SignRequest $signRequest, string $email, File $libreSignFile): void {
+		$l = $this->getLangForEmail($email);
+
 		$emailTemplate = $this->mailer->createEMailTemplate('settings.TestEmail');
 		// TRANSLATORS The subject of the email that is sent when a signature request has been canceled.
-		$emailTemplate->setSubject($this->l10n->t('LibreSign: A signature request has been canceled'));
+		$emailTemplate->setSubject($l->t('LibreSign: A signature request has been canceled'));
 		$emailTemplate->addHeader();
-		$emailTemplate->addHeading($this->l10n->t('Signature request canceled'), false);
+		$emailTemplate->addHeading($l->t('Signature request canceled'), false);
 		// TRANSLATORS The text in the email that is sent when a signature request has been canceled. %s will be replaced with the name of the file.
-		$emailTemplate->addBodyText($this->l10n->t('The request for you to sign "%s" has been canceled.', [$libreSignFile->getName()]));
+		$emailTemplate->addBodyText($l->t('The request for you to sign "%s" has been canceled.', [$libreSignFile->getName()]));
 		$message = $this->mailer->createMessage();
 		if ($signRequest->getDisplayName()) {
 			$message->setTo([$email => $signRequest->getDisplayName()]);
@@ -168,10 +192,12 @@ class MailService {
 	}
 
 	public function sendCodeToSign(string $email, string $name, string $code): void {
+		$l = $this->getLangForEmail($email);
+
 		$emailTemplate = $this->mailer->createEMailTemplate('settings.TestEmail');
-		$emailTemplate->setSubject($this->l10n->t('LibreSign: Code to sign file'));
+		$emailTemplate->setSubject($l->t('LibreSign: Code to sign file'));
 		$emailTemplate->addHeader();
-		$emailTemplate->addBodyText($this->l10n->t('Use this code to sign the document:'));
+		$emailTemplate->addBodyText($l->t('Use this code to sign the document:'));
 		$emailTemplate->addBodyText($code);
 		$message = $this->mailer->createMessage();
 		if (!empty($name)) {
